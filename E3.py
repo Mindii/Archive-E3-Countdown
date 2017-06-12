6eda9de1129f90debd08c670bed598236f32f818
Mindi <mc@mindinet.org>
2017-06-12 04:23:28 +0300

    Nintendo + Elite Dangerous
    Added nintendo and elite dangerous events

--- E3.py
+++ E3.py
@@ -1,6 +1,6 @@
 ########################################
 # E3 Countdown
-# Countdown to next E3 Conference
+# Countdown to next E3 Event
 # Python 3
 # Mindi - contact@mindinet.org
 # Best way to watch: http://twitch.tv/totalbiscuit
@@ -25,10 +25,8 @@ def CountdownToDate(Date="0", Event="none"):
             return "%s: %dmin %dsec" %(Event, minutes, seconds)
         else:
             return "%s: %dh %dmin %dsec" %(Event, hours, minutes, seconds)
-			
     elif days > 1:
         return "%s: %ddays %dh %dmin %dsec" %(Event, days, hours, minutes, seconds)
-		
     else:
         return "%s: %dday %dh %dmin %dsec" %(Event, days, hours, minutes, seconds)
 
@@ -39,7 +37,9 @@ def NextE3():
     '2017-06-11 23:45:00',
     '2017-06-12 06:45:00',
     '2017-06-12 22:45:00',
-    '2017-06-13 03:45:00'
+    '2017-06-13 03:45:00',
+    '2017-06-13 19:00:00',
+    '2017-06-13 21:00:00'
     ];
 	
 	# Simple list for event names.
@@ -48,23 +48,22 @@ def NextE3():
     'MS Conference',
     'Bethesda Conference',
     'UBI Conference',
-    'Sony Conference'
+    'Sony Conference',
+    'Nintendo Spotlight',
+    'Elite Dangerous E3 Live'
     ];
 	
 	# While for checking if there is next event.
     count = 0
-    while (count < 5):
+    while (count < len(TimeList)):
         Date = datetime.strptime(TimeList[count], '%Y-%m-%d %H:%M:%S')
         TimeCheck = (Date - datetime.now()).days * 24 * 3600 + (Date - datetime.now()).seconds
-
         if TimeCheck > 0:
             return(CountdownToDate(Date, EventList[count]))
             sys.exit()
-	
         count = count + 1
 
-E3Event = NextE3()
-if E3Event != None:
-    print(E3Event)
+if NextE3() != None:
+    print(NextE3())
 else:
     print("E3 is over")
